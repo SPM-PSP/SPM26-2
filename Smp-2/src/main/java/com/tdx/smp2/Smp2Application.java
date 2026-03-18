d832de2ac1dbe805a81aadd079aacfb4f6ad89a2
Frank153266 <franky.12138.Henry@gmail.com>
2026-03-18 09:03:40 +0800

    测试3

---
+++ Smp-2/src/main/java/com/tdx/smp2/Smp2Application.java
@@ -0,0 +1,13 @@
+package com.tdx.smp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Smp2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Smp2Application.class, args);
+    }
+
+}
---
+++ Smp-2/src/test/java/com/tdx/smp2/Smp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdx.smp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Smp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
