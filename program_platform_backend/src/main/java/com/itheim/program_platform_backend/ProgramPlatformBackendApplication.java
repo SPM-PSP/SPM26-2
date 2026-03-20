abd7009eb1a065891c8f54b4b4eaf70614c0f7d8
qq501552580 <501552580@qq.com>
2026-03-20 12:53:30 +0800

    后端项目创建

---
+++ program_platform_backend/src/main/java/com/itheim/program_platform_backend/ProgramPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.itheim.program_platform_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ program_platform_backend/src/test/java/com/itheim/program_platform_backend/ProgramPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheim.program_platform_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
